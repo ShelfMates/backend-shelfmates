d49fe6b62e44c36bcb5eea2240d1fa12f34f0440
KaiWelu <kaiweluda@gmail.com>
2025-08-22 10:29:12 +0200

    maven setup

---
+++ src/main/java/com/dci/shelfmates/backend_shelfmates/BackendShelfmatesApplication.java
@@ -0,0 +1,13 @@
+package com.dci.shelfmates.backend_shelfmates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendShelfmatesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendShelfmatesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dci/shelfmates/backend_shelfmates/BackendShelfmatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dci.shelfmates.backend_shelfmates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendShelfmatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
